dd5dee01a84865a7d15ab4a5cb8d4aad2a695e4f
Luqni Maulana <luqni@skyshi.com>
2024-01-03 22:34:28 +0700

    feat: add button icon reusable component

---
+++ src/components/ui/ButtonIcon.vue
@@ -0,0 +1,53 @@
+<template>
+  <button 
+    :type="type"
+    :class="`todo-button-icon ${colorClass} ${sizeClass}`"
+    :disabled="disabled"
+  >
+    <slot/>
+  </button>
+</template>
+<script>
+const buttonColorClasses = {
+  primary: 'todo-button-icon-primary',
+}
+
+const buttonSizeClasses = {
+  sm: 'todo-button-icon-small',
+  md: 'todo-button-icon-medium',
+  lg: 'todo-button-icon-large'
+}
+
+export default {
+  name: 'ButtonIcon',
+  props: {
+    type: {
+      type: String,
+      required: true
+    },
+    color: {
+      type: String,
+      required: false,
+      default: ''
+    },
+    size: {
+      type: String,
+      required: false,
+      default: 'md'
+    },
+    disabled: {
+      type: Boolean,
+      required: false,
+      default: false
+    }
+  },
+  computed: {
+    colorClass() {
+      return buttonColorClasses?.[this.color] || ''
+    },
+    sizeClass() {
+      return buttonSizeClasses?.[this.size] || ''
+    }
+  }
+}
+</script>
