836913b1243dc3aeb04906db740e880c660c3bfd
Luqni Maulana <luqni@skyshi.com>
2024-01-04 06:38:51 +0700

    feat: add input reusable component

---
+++ src/components/ui/Input.vue
@@ -0,0 +1,31 @@
+<template>
+  <input 
+    :type="type"
+    :placeholder="placeholder"
+    :disabled="disabled"
+    :class="`todo-input`"
+  />
+</template>
+<script>
+
+export default {
+  name: 'Input',
+  props: {
+    type: {
+      type: String,
+      required: false,
+      default: 'text'
+    },
+    placeholder: {
+      type: String,
+      required: false,
+      default: ''
+    },
+    disabled: {
+      type: Boolean,
+      required: false,
+      default: false
+    }
+  }
+}
+</script>
