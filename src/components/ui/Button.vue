0661baf115e8412c448c6ac7fbb67b1443f556eb
Luqni Maulana <luqni@skyshi.com>
2024-01-03 22:09:01 +0700

    feat: button reusable component

---
+++ src/components/ui/Button.vue
@@ -0,0 +1,53 @@
+<template>
+  <button 
+    type="button"
+    :class="`todo-button ${colorClass} ${sizeClass}`"
+    :disabled="disabled"
+  >
+    button
+  </button>
+</template>
+<script>
+const buttonColorClasses = {
+  primary: 'todo-button-primary',
+}
+
+const buttonSizeClasses = {
+  sm: 'todo-button-small',
+  md: 'todo-button-medium',
+  lg: 'todo-button-large'
+}
+
+export default {
+  name: 'Button',
+  props: {
+    type: {
+      type: String,
+      required: true
+    },
+    color: {
+      type: String,
+      required: false,
+      default: ''
+    },
+    size: {
+      type: String,
+      required: false,
+      default: 'md'
+    },
+    disabled: {
+      type: Boolean,
+      required: false,
+      default: false
+    }
+  },
+  computed: {
+    colorClass() {
+      return buttonColorClasses?.[this.color] || ''
+    },
+    sizeClass() {
+      return buttonSizeClasses?.[this.size] || ''
+    }
+  }
+}
+</script>
