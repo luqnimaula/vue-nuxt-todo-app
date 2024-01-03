ede8c42d02bbce47d773fbef7408f2e0ee384f88
Luqni Maulana <luqni@skyshi.com>
2024-01-04 06:56:59 +0700

    feat: add checkbox reusable component

---
+++ src/components/ui/Checkbox.vue
@@ -0,0 +1,39 @@
+<template>
+  <div :class="`todo-checkbox ${disabled ? 'disabled' : ''}`">
+    <input 
+      type="checkbox"
+      :id="id"
+      :checked="checked"
+      @change="emit('change')"
+    />
+    <label :for="id">
+      {{ label }}
+    </label>
+  </div>
+</template>
+<script>
+
+export default {
+  name: 'Checkbox',
+  props: {
+    id: {
+      type: String,
+      required: false,
+    },
+    label: {
+      type: String,
+      required: true
+    },
+    checked: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    disabled: {
+      type: Boolean,
+      required: false,
+      default: false
+    }
+  }
+}
+</script>
