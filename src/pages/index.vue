164df1067ef7fdec4abe1a35aa85a8e14bdd8370
Luqni Maulana <luqni@skyshi.com>
2024-01-03 22:08:31 +0700

    fix: main page class

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,3 +1,12 @@
 <template>
-  <div class="todo-container">[main page goes here]</div>
+  <div class="todo-container">
+    <div class="todo-card">
+      [main page goes here]
+    </div>
+  </div>
 </template>
+<script>
+export default {
+  name: 'HomePage',
+}
+</script>
