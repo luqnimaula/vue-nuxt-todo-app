edcea331c4820244ee23203d96cb7757d45d7b4f
Luqni Maulana <luqni@skyshi.com>
2024-01-04 08:22:47 +0700

    ui: fix ui header & todo input form

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,12 +1,31 @@
 <template>
   <div class="todo-container">
     <div class="todo-card">
-      [main page goes here]
+      <div class="todo-header">
+        Todo App
+      </div>
+      <form class="todo-form">
+        <todo-input placeholder="Add a new todo"/>
+        <todo-button 
+          type="submit"
+          color="primary"
+          size="md"
+        >
+          Add
+        </todo-button>
+      </form>
     </div>
   </div>
 </template>
 <script>
+import TodoButton from '~/components/ui/Button.vue'
+import TodoInput from '~/components/ui/Input.vue'
+
 export default {
   name: 'HomePage',
+  components: {
+    TodoButton,
+    TodoInput
+  }
 }
 </script>
